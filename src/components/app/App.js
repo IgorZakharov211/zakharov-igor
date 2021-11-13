6b327c2e9c105039cd5aad9bf5e761d88b241554
Ïgor Zakharov <Igor1994294@yandex.ru>
2021-11-13 22:16:51 +0300

    feat: add routes and some styles

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import './App.scss';
+import Header from '../header/Header';
+import About from '../about/About';
+import Projects from '../projects/Projects';
+import Techs from '../techs/Techs';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="app">
-      
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<About />}></Route>
+        <Route exact path="/projects" element={<Projects />}></Route>
+        <Route exact path="/techs" element={<Techs />}></Route>
+      </Routes>
     </div>
   );
 }
