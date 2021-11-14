f1d49d53ed050377c4e9e41ad7be28bd209a39ea
Ïgor Zakharov <Igor1994294@yandex.ru>
2021-11-14 14:00:03 +0300

    feat: add context

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,21 +1,31 @@
+import React from 'react';
+import { useState } from 'react';
 import './App.scss';
 import Header from '../header/Header';
 import About from '../about/About';
 import Projects from '../projects/Projects';
 import Techs from '../techs/Techs';
 import { Routes, Route } from 'react-router-dom';
-
+import { ThemeContext } from '../../themeContext';
 
 function App() {
+  const [ theme, setTheme ] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme( theme => ( theme === "light" ? "dark" : "light" ));
+  }
+
   return (
-    <div className="app">
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<About />}></Route>
-        <Route exact path="/projects" element={<Projects />}></Route>
-        <Route exact path="/techs" element={<Techs />}></Route>
-      </Routes>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<About />}></Route>
+          <Route exact path="/projects" element={<Projects />}></Route>
+          <Route exact path="/techs" element={<Techs />}></Route>
+        </Routes>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
