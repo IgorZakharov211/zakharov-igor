c7cf347ba40166af4d6eb695411f17588d58d3b6
Ïgor Zakharov <Igor1994294@yandex.ru>
2021-11-15 12:50:12 +0300

    feat: add biography and change props

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './About.scss';
 import MyInfo from '../my-info/MyInfo';
+import Biography from '../biography/Biography';
 
 function About(){
   return(
     <main className="about">
-      <MyInfo />
+      <MyInfo className="about"/>
+      <Biography className={"about"}/>
     </main>
   )
 }
