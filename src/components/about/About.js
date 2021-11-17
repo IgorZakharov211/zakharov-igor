5c377e55e5029ba2084bc41604e7acf0d2b1edd2
Ïgor Zakharov <Igor1994294@yandex.ru>
2021-11-17 12:30:00 +0300

    feat: add hobby

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,14 +4,16 @@ import MyInfo from '../my-info/MyInfo';
 import Biography from '../biography/Biography';
 import Courses from '../courses/Courses';
 import Info from '../info/Info';
+import Hobby from '../hobby/Hobby';
 
 function About(){
   return(
     <main className="about">
-      <MyInfo className="about"/>
-      <Biography className={"about"}/>
-      <Courses className={"about"} />
-      <Info className={"about"} />
+      <MyInfo className="about" />
+      <Biography className="about" />
+      <Courses className="about" />
+      <Info className="about" />
+      <Hobby classNaem="hobby" />
     </main>
   )
 }
