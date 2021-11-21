bd1e94b5dc83d2e1932fd6b86315ca11433368e4
Ïgor Zakharov <Igor1994294@yandex.ru>
2021-11-21 22:06:11 +0300

    feat: add icons to techsList

--- src/components/courses/Courses.js
+++ src/components/courses/Courses.js
@@ -5,21 +5,104 @@ import './Courses.scss';
 import ArrowIcon from '../../images/icons/arrow.svg';
 import { ThemeContext } from "../../themeContext";
 
+
+import HTMLIcon from '../../images/icons/techs/html.png';
+import CSSIcon from '../../images/icons/techs/css.png';
+import JSIcon from '../../images/icons/techs/javascript.png';
+import ReactIcon from '../../images/icons/techs/react.png';
+import GITIcon from '../../images/icons/techs/git.png';
+import darkGITIcom from '../../images/icons/techs/dark-git.png';
+import NodeIcon from '../../images/icons/techs/nodejs.png';
+import ExpressIcon from '../../images/icons/techs/express.png';
+import darkExpressIcon from '../../images/icons/techs/dark-express.png';
+import mongoDBIcon from '../../images/icons/techs/mongodb.png';
+import VSCodeIcon from '../../images/icons/techs/vscode.png';
+import WebpackIcon from '../../images/icons/techs/webpack.png';
+import lightWebpackIcon from '../../images/icons/techs/light-webpack.png';
+import GithubIcon from '../../images/icons/techs/github.svg';
+import darkGithubIcon from '../../images/icons/techs/dark-github.svg';
+import SassIcon from '../../images/icons/techs/sass.png';
+
+
+
 function Courses(props){
 
   const { theme } = useContext(ThemeContext);
-  const coursesList = ["HTML", "CSS", "JavaScript", "React", "GIT", "Express.js", "mongoDB"]
+  
+  const techsList = [
+    {
+      key: 1,
+      alt: "HTML",
+      src: HTMLIcon
+    },
+    {
+      key: 2,
+      alt: "CSS",
+      src: CSSIcon
+    },
+    {
+      key: 3,
+      alt: "JavaScript",
+      src: JSIcon
+    },
+    {
+      key: 4,
+      alt: "React",
+      src: ReactIcon
+    },
+    {
+      key: 5,
+      alt: "GIT",
+      src: (theme === "light" ? GITIcon : darkGITIcom)
+    },
+    {
+      key: 6, 
+      alt: "Node.js",
+      src: NodeIcon
+    },
+    {
+      key: 7,
+      alt: "Express.js",
+      src: (theme === "light" ? ExpressIcon : darkExpressIcon)
+    },
+    {
+      key: 8, 
+      alt: "mongoDB",
+      src: mongoDBIcon
+    },
+    {
+      key: 9,
+      alt: "VSCode",
+      src: VSCodeIcon
+    },
+    {
+      key: 10, 
+      alt: "Webpack",
+      src: (theme === "light" ? lightWebpackIcon : WebpackIcon)
+    },
+    {
+      key: 11, 
+      alt: "GitHub",
+      src: (theme === "light" ? GithubIcon : darkGithubIcon)
+    },
+    {
+      key: 12,
+      alt: "Sass",
+      src: SassIcon
+    }
+  ];
+
 
   return(
     <section className={`courses ${props.className}__courses ${theme === "light" ? "" : "courses_dark"}`}>
       <div className="courses__container">
       <h3 
         className={`title courses__title ${theme === "light" ? "" : "title_dark"}`}>
-        Технологии освоенные на курсах</h3>
+        Освоенные технологии и инструменты</h3>
       <ul className="courses__list">
         {
-        coursesList.map((i) => {
-          return CoursesItem(i);
+        techsList.map((i) => {
+          return CoursesItem({key: i.key, src: i.src, alt: i.alt});
         })
        }
       </ul>
@@ -32,12 +115,14 @@ function Courses(props){
   )
 }
 
-function CoursesItem(name){
+function CoursesItem({key, src, alt}){
 
   const { theme } = useContext(ThemeContext);
 
   return(
-    <li key={name} className={`courses__item ${theme === "light" ? "" : "courses__item_dark"}`}>{name}</li>
+    <li key={key} className={`courses__item ${theme === "light" ? "" : "courses__item_dark"}`}>
+      <img className="courses__img" src={src} alt={alt} />
+    </li>
   )
 }
 
