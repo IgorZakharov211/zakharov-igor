6cbdfd12e88dd1d73f8b60681b1174e3eef03cf5
Ïgor Zakharov <Igor1994294@yandex.ru>
2021-11-26 18:32:14 +0300

    feat: add mobile logo

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -4,6 +4,8 @@ import './Logo.scss';
 import { Link } from 'react-router-dom';
 import LogoImg from '../../images/logo.svg';
 import DarkLogoImg from '../../images/logo-dark.svg';
+import mobileLogoImg from '../../images/mobileLogo.svg';
+import darkMobileLogoImg from '../../images/mobileLogo-dark.svg';
 import { ThemeContext } from '../../themeContext';
 
 
@@ -13,6 +15,9 @@ function Logo(){
     <div className="logo">
       <Link to="/" className="logo__link">
         <img src={theme === "light" ? LogoImg : DarkLogoImg } alt="Захаров Игорь(логотип)" className="logo__img"/>
+        <div className={`logo__container ${theme === "light" ? "" : "logo__container_dark"}`}>
+        <img src={theme === "light" ? mobileLogoImg : darkMobileLogoImg} alt="Захаров Игорь(логотип)" className="logo__mobile-img" />
+        </div>
       </Link> 
     </div>
   )
